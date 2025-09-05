2b4a02f149004dfb4fa9e190feec97b345b12a27
Kamil Jan Mularski <kammul@outlook.com>
2025-09-05 22:46:15 +0200

    Solution in the night

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,25 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        String read = "";
+        try {
+            read = Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read the file", e);
+        }
+        String[] split = read.split("[\\s\\p{P}]+");
+        Arrays.sort(split, Comparator.naturalOrder());
+        for (int i = 0; i < split.length; i++) {
+            split[i] = split[i].toLowerCase();
+        }
+        return split;
     }
 }
